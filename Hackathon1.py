0943f3cd8235c985327c2c0e62586d3c4a0d6886
Devansh Pratap Singh <53232548+devanshpratapsingh@users.noreply.github.com>
2020-10-07 09:55:49 +0530

    Update Hackathon1.py

--- Hackathon1.py
+++ Hackathon1.py
@@ -18,3 +18,21 @@ model.add(tf.keras.layers.Flatten(input_shape=(82,28)))  #flatten means it's a s
 model.add(tf.keras.layers.Dense(units=128, activation=tf.nm.relu))  #dense means all the neurons are connected to
 model.add(tf.keras.layers.Dense(units=128, activation=tf.nm.relu))  #previous and the next layer
 model.add(tf.keras.layers.Dense(units=10, activation=tf.nm.softmax))
+
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+loss, accuracy= model.evaluate(x_test, y_test)
+print(accuracy)
+print(loss)
+
+model.save('digits.model')
+
+
+for x in range(1,6):
+    img = cv.imread(f'{x}.jpeg')[:,:,0]
+    img = np.invert(np.array([img]))
+    prediction = model.predict(img)
+    print(f'Probably the result is: {np.argmax(prediction)}')
+    plt.imshow(img[0], cmap=plt.cm.binary)
+    plt.show()
